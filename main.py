0145c4d04e80a15015ad7062cd960f9e450029df
Jiawei Tong <tong0514@yahoo.com>
2022-12-06 17:45:38 -0800

    finished project

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request,url_for, redirect, make_response,flash
 from google.cloud.sql.connector import Connector
-import pymysql
-import sqlalchemy 
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, SelectField, SelectMultipleField,widgets
@@ -134,34 +132,26 @@ class CriteriaForm(FlaskForm):
     '18:00','19:00','20:00','21:00','22:00','23:00','00:00'))
     submit = SubmitField('Search')  
 
-# class ProfessorForm(FlaskForm):
-#     name = StringField(render_kw={"placeholder": "name"})
-#     field = SelectField('filed', choices=('teaching courses','working field'))
-
-# class CourseTable(Table)
-
+# login details
 @app.route("/")
 def homepage(methods = ['GET', 'POST']):
-    name = 'Spring 2023'
-    semester_courses = Course.query.filter_by(semester = 'Spring 2023',campus_name = 'Boston',credit = 4)
-    # # (test db connection purpose)response list all data in a table
 
-    response = list()
+    # response = list()
 
-    # response.append({
-    #         "name" : student.name,
-    #         "major": student.major
+    # # response.append({
+    # #         "name" : student.name,
+    # #         "major": student.major
+    # #     })
+    # for course in semester_courses:
+    #     response.append({
+    #         "name" : course.name,
+    #         "total_wl": course.total_wl,
+    #         "level": course.level
     #     })
-    for course in semester_courses:
-        response.append({
-            "name" : course.name,
-            "total_wl": course.total_wl,
-            "level": course.level
-        })
-    return make_response({
-        'status' : 'success',
-        'message': response
-    }, 200)
+    # return make_response({
+    #     'status' : 'success',
+    #     'message': response
+    # }, 200)
     form = LoginForm()
     return render_template("home.html", form = form)
 
@@ -207,6 +197,7 @@ def adminDashboard():
     return render_template('adminDashboard.html')
 
 
+# student dashboard
 @app.route('/criteria', methods=['GET','POST'])
 def criteria():
     form = CriteriaForm()
@@ -216,9 +207,69 @@ def criteria():
     campus.insert(0, "")
     form.major.choices = major
     form.campus.choices = campus
-    return render_template('criteria.html',form = form)    
+    return render_template('criteria.html',form = form)  
+
+@app.route('/filter', methods=['GET','POST'])
+def filter():
+    form = CriteriaForm()
+    major = [x[0] for x in Major.query.with_entities(Major.name)]
+    major.insert(0, "")
+    campus = [x[0] for x in Campus.query.with_entities(Campus.name)]
+    campus.insert(0, "")
+    form.major.choices = major
+    form.campus.choices = campus
+    return render_template('browse_criteria.html',form = form)  
 
 @app.route('/search',  methods=['GET','POST'])
+def search():
+    form = request.form
+    semester = form['semester']
+    number = form['number'] 
+    CRN = form['CRN']
+    major = form['major']  
+    campus = form['campus'] 
+    keyword = form['keyword'] 
+    type = form['type'] 
+    level = form['level']
+    days = request.form.getlist('days')
+    time_from = form['time_from'] 
+    time_to = form['time_to'] 
+    equal_conditions = [('number',number),('CRN',CRN),('major',major),('campus_name',campus),
+    ('type',type),('level',level)] * 1
+
+    valid_equal_conditions = list()
+    valid_other_conditions = list()
+    for row in equal_conditions:
+        if (row[1] == '' or row[1] == ' ' or row[1] is None or not row[1]) == False:
+            valid_equal_conditions.append(row) 
+    for row in equal_conditions:
+        if (row[1] == '' or row[1] == ' ' or row[1] is None or not row[1]) == False:
+            valid_other_conditions.append(row)         
+    query_string = ('select * from course where semester = "{s}"').format(s=semester)
+    for row in valid_equal_conditions:
+        if row[0] == 'CRN':
+            query_string += (' and {a} = {b}').format(a = row[0],b = row[1])
+        else:
+            query_string += (" and {a} = '{b}'").format(a = row[0],b = row[1])
+    if keyword != '':
+        query_string += (" and name like '%{k}%'").format(k = keyword)
+    if not days:
+        for day in days:
+            query_string += (" and days like '{d}'").format(d = day)    
+    if time_from != '':
+        query_string += (" and time_from >= '{f}'").format(f = time_from)
+    if time_to != '':
+        query_string += (" and time_to <= '{t}'").format(t = time_to)  
+    if len(days) > 0:
+        for day in days:
+            query_string += (" and days like '%{d}%'").format(d = day)   
+    # SELECT * FROM course WHERE days LIKE '%FRI%' and days LIKE '%WED%';
+    print(query_string)   
+    courses = db.session.execute(select(Course).from_statement(text(query_string))).scalars().all()
+
+    return render_template('browseClasses.html',courses = courses)   
+
+@app.route('/searchCourse',  methods=['GET','POST'])
 def searchCourse():
     form = request.form
     semester = form['semester']
@@ -264,19 +315,8 @@ def searchCourse():
     # SELECT * FROM course WHERE days LIKE '%FRI%' and days LIKE '%WED%';
     print(query_string)   
     courses = db.session.execute(select(Course).from_statement(text(query_string))).scalars().all()
-    # response = list()
-    # for course in courses:
-    #     response.append({
-    #         "name" : course.name,
-    #         "number": course.number,
-    #         "major": course.major
-    #     })
-    # return make_response({
-    #     'status' : 'success',
-    #     'message': response
-    # }, 200)
 
-    return render_template('browseClasses.html',courses = courses)   
+    return render_template('classes.html',courses = courses)   
 
 @app.route('/register',  methods=['POST'])
 def register():
@@ -291,15 +331,84 @@ def register():
     registration = Registration(crn=crn, nuid=nuid, course_number=course_number, permission=permission, registration_time=registration_time)
     db.session.add(registration)
     db.session.commit()
-    print(crn);
+    return(redirect('/semester'))
     return "Success", 200, {"Access-Control-Allow-Origin": "*"}
 
 @app.route('/semester', methods=['GET','POST'])
 def semester():
-    nuid = current_user.get_id()
-    myCourses = db.session.execute(select(Registration).filter_by(nuid=nuid)).scalars().all()
-    terms = ['Spring 2023','Fall 2022','Summer 2022']
-    return render_template('semester.html', myCourses=myCourses, terms=terms)
+    id = current_user.get_id()
+    # reg = Registration.query.join(Student).filter(Registration,nuid = Registration.nuid)
+
+    regs = db.session.query(
+         Registration.crn, Registration.course_number,Course.name, Registration.nuid, Registration.registration_time, Registration.permission
+    ).filter(
+         Registration.crn == Course.crn
+    ).filter_by(
+         nuid = id
+    ).all()
+    print(regs)
+
+    # myCourses = db.session.execute(select(Registration).filter_by(nuid=nuid)).scalars().all()
+    # terms = ['Spring 2023','Fall 2022','Summer 2022']
+    return render_template('student_view.html', regs=regs)
+
+@app.route('/dropClass', methods = ['POST'])
+def dropClass():
+    form = request.form
+    crn = form['crn']
+    nuid = form['nuid']
+    Registration.query.filter_by(crn=crn, nuid=nuid).delete()
+    # print(reg.permission)
+    # reg.permission = permission
+    db.session.commit()
+    return(redirect('/semester'))
+# admin dashboard
+@app.route('/selectSemester', methods=['GET','POST'])
+def selectSemester():
+    terms = ['','Spring 2023','Fall 2022','Summer 2022']
+    return render_template('adminSemester.html', list=terms)
+
+@app.route('/selectSemester1', methods=['GET','POST'])
+def selectSemester1():
+    terms = ['','Spring 2023','Fall 2022','Summer 2022']
+    return render_template('adminSemester1.html', list=terms)    
+
+@app.route('/viewRegistration', methods=['GET','POST'])
+def viewRegistration():
+    semester = request.form.get('semester')
+    regs = db.session.query(
+         Registration.crn, Registration.course_number,Course.name,Student.name, Registration.nuid, Registration.registration_time, Registration.permission
+    ).filter(
+         Registration.crn == Course.crn,
+    ).filter(
+         Registration.nuid == Student.nuid,
+    ).all()
+    return render_template('viewRegistration.html',semester = semester,regs = regs)    
+
+@app.route('/editPermission', methods=['GET','POST'])
+def editPermission():
+    semester = request.form.get('semester')
+    regs = db.session.query(
+         Registration.crn, Registration.course_number,Course.name,Student.name, Registration.nuid, Registration.registration_time, Registration.permission
+    ).filter(
+         Registration.crn == Course.crn,
+    ).filter(
+         Registration.nuid == Student.nuid,
+    ).all()
+    return render_template('editPermission.html',semester = semester,regs = regs)   
+
+@app.route('/updatePermission', methods = ['POST'])
+def updatePermission():
+    form = request.form
+    crn = form['crn']
+    nuid = form['nuid']
+    permission = form['permission']
+    reg = Registration.query.filter_by(crn=crn, nuid=nuid).first()
+    print(reg.permission)
+    reg.permission = permission
+    db.session.commit()
+
+    return(redirect('/editPermission'))
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8080, debug=True)
